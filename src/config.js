43e14e04a53ec02be16f4ad7ee05be8143e7e27e
Cristian Ramirez <cristian25ramirezrc@gmail.com>
2024-09-07 11:23:12 -0400

    agregado del conteo de productos en el cartmodal

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     API_URL: 'http://localhost/tienda-virtual/tienda-virtual-backend/',
-    WHATSSPP_URL : 'https://wa.me/59173163151'
+    WHATSSPP_URL : 'https://wa.me/59175057788'
   };
   
   export default CONFIG;
