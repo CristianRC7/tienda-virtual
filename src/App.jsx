8fe8f0ecc618f9cb1cb3ce22fef64cd66ec543d6
Cristian Ramirez <cristian25ramirezrc@gmail.com>
2024-09-12 17:18:02 -0400

    estructura para admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import './App.css'
-import NavBar from './components/Navbar'
-import HeroSection from './pages/HeroSection'
-import AboutSection from './pages/AboutSection'
-import ProductsSection from './pages/ProductsSection'
-import Footer from './pages/Footer'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/Navbar';
+import HeroSection from './pages/HeroSection';
+import AboutSection from './pages/AboutSection';
+import ProductsSection from './pages/ProductsSection';
+import Footer from './pages/Footer';
+import AdminLogin from './admin/AdminLogin'; 
+import Dashboard from './admin/pages/Dashboard'; 
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />} />
+
+        <Route path="/admin/" element={<AdminLogin />} />
+        <Route path="/admin/pages/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function MainLayout() {
   return (
     <>
       <NavBar />
@@ -17,7 +31,7 @@ function App() {
       </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tienda-virtual-backend/login.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'conexion.php';
+include_once 'cors.php';
+
+$input = json_decode(file_get_contents("php://input"), true);
+$usuario = $input['usuario'];
+$contrasena = $input['contrasena'];
+
+$sql = "SELECT * FROM usuarios WHERE usuario = ? AND contrasena = ?";
+$stmt = $conexion->prepare($sql);
+$stmt->bind_param('ss', $usuario, $contrasena);
+$stmt->execute();
+$resultado = $stmt->get_result();
+
+if ($resultado->num_rows > 0) {
+    echo json_encode(['success' => true]);
+} else {
+    // Credenciales incorrectas
+    echo json_encode(['success' => false]);
+}
+
+$stmt->close();
+$conexion->close();
+?>
